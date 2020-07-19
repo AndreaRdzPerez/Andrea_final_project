fde6cd8f62aa1656c41ffdddfca19076942a1696
AndreaRdzPerez <51881798+AndreaRdzPerez@users.noreply.github.com>
2020-07-19 19:50:14 +0200

    Created field service

---
+++ field-service/src/main/java/com/ironhack/fieldservice/controller/FieldController.java
@@ -0,0 +1,32 @@
+package com.ironhack.fieldservice.controller;
+
+import com.ironhack.fieldservice.model.Field;
+import com.ironhack.fieldservice.service.FieldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FieldController {
+    @Autowired
+    private FieldService fieldService;
+
+    // READ
+
+    /**
+     * Find a Field by Id.
+     *
+     * @param id Receives the Id of the Field for searching by Param.
+     * @return Returns the Field with the Id given.
+     * @throws DataNotFoundException a Exception
+     */
+    @GetMapping("/field/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Field findById(@PathVariable Integer id) {
+        return fieldService.findById(id);
+    }
+
+
+}
