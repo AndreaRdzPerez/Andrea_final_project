5d141cc60661ebae53f4e2d681de2296be248332
AndreaRdzPerez <51881798+AndreaRdzPerez@users.noreply.github.com>
2020-07-24 23:10:29 +0200

    Modified data file from user-service

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -1,12 +1,8 @@
 insert into users(username, password) values
-('referee', '$2a$10$P.EDNKPW25.IQyqFwVQ5.uwBfFEctsRuAFojgq1PFJh3k/3DPSMh2'),
-('teamCaptain', '$2a$10$P.EDNKPW25.IQyqFwVQ5.uwBfFEctsRuAFojgq1PFJh3k/3DPSMh2'),
+('TeamUser', '$2a$10$P.EDNKPW25.IQyqFwVQ5.uwBfFEctsRuAFojgq1PFJh3k/3DPSMh2'),
 ('admin','$2a$10$P.EDNKPW25.IQyqFwVQ5.uwBfFEctsRuAFojgq1PFJh3k/3DPSMh2');
 
 insert into role (role, user_id) values
-('ROLE_REFEREE', 1),
-('ROLE_TEAM', 1),
-('ROLE_TEAM', 2),
-('ROLE_REFEREE', 3),
-('ROLE_TEAM', 3),
-('ROLE_ADMIN', 3);
+('ROLE_USER', 1),
+('ROLE_USER', 2),
+('ROLE_ADMIN', 2);
