a912c615c464e9db22b63c1c6692db3a26c0405e
AndreaRdzPerez <51881798+AndreaRdzPerez@users.noreply.github.com>
2020-07-24 21:37:51 +0200

    front

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -16,8 +15,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BasicAuthInterceptor, ErrorInterceptor } from './_helpers';
-
-
 
 @NgModule({
   declarations: [
--- front/src/app/referees/referees.component.ts
+++ front/src/app/referees/referees.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Match } from '../models/matches';
+import { Referee } from '../models/referees';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../_services';
 import { first } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-referees',
@@ -13,10 +14,34 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./referees.component.css']
 })
 export class RefereesComponent implements OnInit {
+  username: 'admin';
+  password: 'admin';
 
-  constructor() { }
+    // authorizationData = 'Basic ' + btoa('salesrep:admin');
+
+    httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${this.authenticationService.userValue.authdata}`,
+      }),
+    };
+  
+    referees: Referee[] = [];
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.http
+    .get<Referee[]>(`${environment.apiURL}/referees`, this.httpOptions)
+    .pipe(first())
+    .subscribe((data) => {
+      console.log(data);
+      this.referees = data;
+    });
   }
 
 }
